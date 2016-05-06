5f769111c3467add715deffc1c186ace3da978d1
Mike Leonard <mike.r.leonard@gmail.com>
2016-05-06 14:40:35 +1000

    Remove redundant code from graphqlMiddleware.

--- src/redux/middleware/graphqlMiddleware.js
+++ src/redux/middleware/graphqlMiddleware.js
@@ -1,25 +1,29 @@
 const graphqlMiddleware = store => next => action => {
   next(action);
 
-  if (action.meta && action.meta.query) {
+  if (action.meta && action.meta.graphql) {
     let options;
+
+    let graphqlUrl = 'http://localhost:8080/graphql';
+
     options = {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
+      credentials: 'include',
       body: JSON.stringify({
-        query: action.meta.query,
-        variables: action.meta.variables,
+        query: action.meta.graphql.query,
+        variables: action.meta.graphql.variables,
       }),
     };
-    fetch('http://localhost:8080/graphql', options)
+    fetch(graphqlUrl, options)
     .then(function (response) {
       return response.json();
     }).then(function (json) {
       if (json.status === 'error') {
-        if (action.meta.error) {
-          var errorResult = action.meta.error(json, action.payload);
+        if (action.meta.graphql.error) {
+          var errorResult = action.meta.graphql.error(json, action.payload);
           // Check to see if the result has a type attribute, if it does then
           // assume its a Redux action and dispatch it.
           if (errorResult && errorResult.type) {
@@ -28,8 +32,8 @@ const graphqlMiddleware = store => next => action => {
         }
         return;
       }
-      if (action.meta.success) {
-        var successResult = action.meta.success(json.data, action.payload);
+      if (action.meta.graphql.success) {
+        var successResult = action.meta.graphql.success(json.data, action.payload);
         // Check to see if the result has a type attribute, if it does then
         // assume its a Redux action and dispatch it.
         if (successResult && successResult.type) {
@@ -39,7 +43,7 @@ const graphqlMiddleware = store => next => action => {
     }).catch(function (ex) {
       console.log('parsing failed', ex);
     });
-    console.log('fetching: ' + action.meta.query);
+    console.log('fetching: ' + action.meta.graphql.query);
   }
 };
 
