7b7d5679e6934207561fc68a2942c5c80701c953
Mike Leonard <mike.r.leonard@gmail.com>
2016-05-06 13:15:12 +1000

    Use generic Loading component when BotList is loading.

--- src/components/BotList.js
+++ src/components/BotList.js
@@ -2,6 +2,7 @@
 import React, { PropTypes } from 'react';
 import BotName from './BotName';
 import BotWinPercent from './BotWinPercent';
+import Loading from './Loading';
 
 export class BotList extends React.Component {
   // props: Props;
@@ -10,42 +11,42 @@ export class BotList extends React.Component {
   };
 
   render () {
+    if (!this.props.bots) {
+      return (
+        <Loading />
+      );
+    }
+
     const bots = () => {
-      if (this.props.bots) {
-        return (
-          <tbody>
-            {
-              this.props.bots.map((b) => {
-                return (
-                  <tr>
-                    <td>
-                      <BotName
-                        id={b.id}
-                        name={b.name}
-                        version={b.version}
-                        />
-                    </td>
-                    <td>{b.status}</td>
-                    <td>{b.gameType.name}</td>
-                    <td>{b.gamesPlayed}</td>
-                    <td>{b.gamesWon}</td>
-                    <td>
-                      <BotWinPercent
-                        gamesPlayed={b.gamesPlayed}
-                        gamesWon={b.gamesWon}
-                        />
-                    </td>
-                  </tr>
-                );
-              })
-            }
-          </tbody>
-        );
-      } else {
-        return (
-          <span>Loading...</span>
-        );
-      }
+      return (
+        <tbody>
+          {
+            this.props.bots.map((b) => {
+              return (
+                <tr key={b.id}>
+                  <td>
+                    <BotName
+                      id={b.id}
+                      name={b.name}
+                      version={b.version}
+                      />
+                  </td>
+                  <td>{b.status}</td>
+                  <td>{b.gameType.name}</td>
+                  <td>{b.gamesPlayed}</td>
+                  <td>{b.gamesWon}</td>
+                  <td>
+                    <BotWinPercent
+                      gamesPlayed={b.gamesPlayed}
+                      gamesWon={b.gamesWon}
+                      />
+                  </td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      );
     };
 
     return (
